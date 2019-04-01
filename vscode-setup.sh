06697427cd3d2e48249942f804c75049920475d5
Quang Ngo <nnquangit@gmail.com>
2019-04-01 18:28:37 +0700

    List plugins for dev

---
+++ vscode-setup.sh
@@ -0,0 +1,32 @@
+#color
+code --install-extension anseki.vscode-color
+code --install-extension bierner.color-info
+code --install-extension naumovs.color-highlight
+code --install-extension lihui.vs-color-picker
+#css
+code --install-extension paulmolluzzo.convert-css-in-js
+#npm
+code --install-extension eg2.vscode-npm-script
+code --install-extension christian-kohler.npm-intellisense
+#eslint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension drKnoxy.eslint-disable-snippets
+code --install-extension dsznajder.es7-react-js-snippets
+#git
+code --install-extension eamodio.gitlens
+code --install-extension mhutchie.git-graph
+#tslint
+code --install-extension eg2.tslint
+code --install-extension flowtype.flow-for-vscode
+#jest
+code --install-extension Orta.vscode-jest
+#docker
+code --install-extension PeterJausovec.vscode-docker
+#react native
+code --install-extension vsmobile.vscode-react-native
+#markdown
+code --install-extension yzhang.markdown-all-in-one
+#graph
+code --install-extension mquandalle.graphql
+code --install-extension Prisma.vscode-graphql
+code --install-extension kumar-harsh.graphql-for-vscode
