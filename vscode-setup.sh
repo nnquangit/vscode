843eb171bde1907121a33cecc861032d5b811c11
Quang Ngo <nnquangit@gmail.com>
2019-04-20 17:21:35 +0700

    Update vscode-setup.sh
    Add extension

--- vscode-setup.sh
+++ vscode-setup.sh
@@ -3,30 +3,40 @@ code --install-extension anseki.vscode-color
 code --install-extension bierner.color-info
 code --install-extension naumovs.color-highlight
 code --install-extension lihui.vs-color-picker
-#css
-code --install-extension paulmolluzzo.convert-css-in-js
-#npm
+#git & npm
 code --install-extension eg2.vscode-npm-script
 code --install-extension christian-kohler.npm-intellisense
-#eslint
+code --install-extension eamodio.gitlens
+code --install-extension mhutchie.git-graph
+code --install-extension donjayamanne.githistory
+#prettier & tslint & eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension eg2.tslint
+code --install-extension flowtype.flow-for-vscode
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension drKnoxy.eslint-disable-snippets
 code --install-extension dsznajder.es7-react-js-snippets
-#git
-code --install-extension eamodio.gitlens
-code --install-extension mhutchie.git-graph
-#tslint
-code --install-extension eg2.tslint
-code --install-extension flowtype.flow-for-vscode
 #jest
 code --install-extension Orta.vscode-jest
-#docker
-code --install-extension PeterJausovec.vscode-docker
-#react native
+#react native & css in js
+code --install-extension paulmolluzzo.convert-css-in-js
 code --install-extension vsmobile.vscode-react-native
-#markdown
-code --install-extension yzhang.markdown-all-in-one
 #graph
 code --install-extension mquandalle.graphql
 code --install-extension Prisma.vscode-graphql
 code --install-extension kumar-harsh.graphql-for-vscode
+#javascript & bootstrap
+code --install-extension WallabyJs.quokka-vscode
+code --install-extension samundrak.esdoc-mdn
+code --install-extension thekalinga.bootstrap4-vscode
+#docker & tools & capture & translate
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension pnp.polacode
+code --install-extension funkyremi.vscode-google-translate
+code --install-extension humao.rest-client
+code --install-extension idleberg.icon-fonts
+code --install-extension cssho.vscode-svgviewer
+#env & shell
+code --install-extension foxundermoon.shell-format
+code --install-extension mikestead.dotenv
